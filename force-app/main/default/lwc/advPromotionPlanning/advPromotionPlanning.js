cb56c3b037790e9abaab3e1c038d44b99e706c83
Timo Verbrugghe <tverbrugghe@salesforce.com>
2022-04-20 11:49:19 +0200

    Add additional CG cloud components + dedicated Slack components

---
+++ force-app/main/default/lwc/advPromotionPlanning/advPromotionPlanning.js
@@ -0,0 +1,5 @@
+import { LightningElement } from 'lwc';
+
+export default class AdvPromotionPlanning extends LightningElement {
+    searchReps = [{'Id': 1, 'Name': 'Timo Verbrugghe'}, {'Id': 2, 'Name': 'Other Rep'}];
+}
--- force-app/main/default/lwc/yourDayTableauCrm/yourDayTableauCrm.js
+++ force-app/main/default/lwc/yourDayTableauCrm/yourDayTableauCrm.js
@@ -1,10 +1,11 @@
 import { LightningElement, api } from 'lwc';
 
 export default class YourDayTableauCrm extends LightningElement {
-    tableauImg1 = 'https://i.ibb.co/2ggDzty/image-1.png';
-    tableauImg2 = this.tableauImg1;
-    tableauImg3 = this.tableauImg1;
-    tableauImg4 = this.tableauImg1;
+    tableauImg1 = 'https://i.ibb.co/Np7mNm0/Screenshot-2022-03-31-at-03-21-35.png';
+    tableauImg2 = 'https://i.ibb.co/44V37Y3/Screenshot-2022-03-31-at-03-23-33.png';
+    tableauImg3 = 'https://i.ibb.co/2ggDzty/image-1.png';
+
+    myKeyAccounts = 'sfanalytics://dashboard/0FK7Q0000003ldPWAQ/?orgId=00D7Q000004uI89&loginHost=login.salesforce.com';
 
     @api CardTitle = "CRM Analytics"
 }
