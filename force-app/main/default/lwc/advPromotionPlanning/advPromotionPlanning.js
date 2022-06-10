b9e8f84538194cdc7f184e0c74f6bbe1e154a3a4
Timo Verbrugghe <tverbrugghe@salesforce.com>
2022-06-10 15:06:35 +0200

    Additional LWCs

--- force-app/main/default/lwc/advPromotionPlanning/advPromotionPlanning.js
+++ force-app/main/default/lwc/advPromotionPlanning/advPromotionPlanning.js
@@ -1,5 +1,16 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 
 export default class AdvPromotionPlanning extends LightningElement {
-    searchReps = [{'Id': 1, 'Name': 'Timo Verbrugghe'}, {'Id': 2, 'Name': 'Other Rep'}];
+    @track
+    searchReps = [{'Id': 'YFDODIF12', 'Name': 'Chantelle', 'Total': 56, 'Store1VisitAmount': 5}, {'Id': 'YFDODIF13', 'Name': 'Jason', 'Total': 56, 'Store1VisitAmount': 6}, {'Id': 'YFDODIF13', 'Name': 'Jordan', 'Total': 56, 'Store1VisitAmount': 6}, {'Id': 'YFDODIF13', 'Name': 'Julien', 'Total': 32, 'Store1VisitAmount': 0}];
+
+    connectedCallback() {
+
+    }
+
+    handleTestChange(event) {
+        let idArray = event.target.id.split("-");
+        let repObject = this.searchReps.find(x => x.Id === idArray[0]);
+        console.log(repObject);
+    }
 }
